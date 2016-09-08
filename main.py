7e6d3744aca2e1031c7ef0492bd756ba6ad0a9c4
Andrew Liu <ahliu@berkeley.edu>
2016-09-08 14:12:38 -0700

    evaluation framework

--- main.py
+++ main.py
@@ -29,81 +29,123 @@ def ROT_MSC(input_image, template, k = np.ones(72)/72.):
 
   return k
 
-def X_MSC_MSC(input_image, template1, hyper_dim=10000, rot_=10):
+def X_MSC_MSC(input_image, template1, hyper_dim=10000, rot_=10, gran=10, dist=5):
   # 1 - all x translations
   # 2 - all y translations
-  k1 = np.ones(10)/10.
-  k2 = np.ones(10)/10.
+
+
+  k1 = np.ones(gran)/float(gran)
+  k2 = np.ones(gran)/float(gran)
   k3 = np.ones(rot_)/float(rot_)
-  forward_super_imposed1 = np.zeros(hyper_dim)
-  forward_super_imposed2 = np.zeros(hyper_dim)
-  r1 = np.zeros((10, hyper_dim))
-  r2 = np.zeros((10, hyper_dim))
+  forward_super_imposed1 = np.zeros(input_image.shape)
+  forward_super_imposed2 = np.zeros(input_image.shape)
+  r1 = np.zeros((gran, hyper_dim))
+  r2 = np.zeros((gran, hyper_dim))
   r3 = np.zeros((rot_, hyper_dim))
 
-  
-  print "l"
   l = input_image.shape[0]*input_image.shape[1]
 
-  k = utils.GenerateRandomBinary(l, hyper_dim)
   deg = 360/rot_
 
   # fn = utils.NCC
   fn = lambda x, y: np.sum(x*y)
 
+  k1 = np.ones(gran)/float(gran)
+  k2 = np.ones(gran)/float(gran)
+  k3 = np.ones(rot_)/float(rot_)
+  k = utils.GenerateRandomBinary(l, hyper_dim)
   # Forward prop
-  for i in range(10):
-    x = np.roll(input_image, (i-5)*8, axis=1)
-    x = np.dot(x.reshape(l), k)
-    print x.shape
-    r1[i] = k1[i]*x
-    forward_super_imposed1 += k1[i]*x
+  for i in range(gran):
+    x_im = np.roll(input_image, (i-gran//2)*dist, axis=1)
+    x = np.dot(x_im.reshape(l), k)
+    r1[i] = x
+    forward_super_imposed1 += x_im
 
-  for i in range(10):
-    y = np.roll(forward_super_imposed1, (i-5)*8, axis=0)
-    r2[i] = k2[i] * y
-    forward_super_imposed2 += k2[i]*y
+  for i in range(gran):
+    y_im = np.roll(forward_super_imposed1, (i-gran//2)*dist, axis=0)
+    y = np.dot(y_im.reshape(l), k)
+    r2[i] = y
+    forward_super_imposed2 += y_im
 
   for i in range(rot_):
-    r3[i] = k3[i]*fonts.RotateImage(forward_super_imposed2, i*deg)
+    r_im = fonts.RotateImage(forward_super_imposed2, i*deg)
+    r = np.dot(r_im.reshape(l), k)
+    r3[i] = r
 
   back_super_imposed2 = np.zeros(input_image.shape)
   back_super_imposed3 = np.zeros(input_image.shape)
+
+  intermediate_template = np.dot(template1.reshape(l), k)
   # Backward prop
   for i in range(rot_):
-    back_super_imposed3 += k3[i] * fonts.RotateImage(template1, i*deg)
-    q = fn(template1, r3[i])
-    print i, q
-    k3[i] = k3[i]*np.exp(q)
+    r_im = fonts.RotateImage(template1, i*deg)
+    back_super_imposed3 += r_im
+    q = fn(intermediate_template, r3[i])
+    k3[i] = k3[i]*q
 
-  for i in range(10):
-    y = np.roll(back_super_imposed3, (i-5)*8, axis=0)
-    back_super_imposed2 += k2[i] * y
-    q = fn(template1, r2[i])
-    k2[i] = k2[i]*np.exp(q)
 
-  for i in range(10):
-    q = fn(back_super_imposed2, r1[i])
-    k1[i] = k1[i]*np.exp(q)
+  intermediate_template = np.dot(back_super_imposed3.reshape(l), k)
+  for i in range(gran):
+    y = np.roll(back_super_imposed3, (i-gran//2)*dist, axis=0)
+    back_super_imposed2 += y
+    q = fn(intermediate_template, r2[i])
+    k2[i] = k2[i]*q
+
+  intermediate_template = np.dot(back_super_imposed2.reshape(l), k)
+  for i in range(gran):
+    q = fn(intermediate_template, r1[i])
+    k1[i] = k1[i]*q
 
   k3 = k3/np.sum(k3)
   k2 = k2/np.sum(k2)
   k1 = k1/np.sum(k1)
 
-  print "Rotation: " + str(np.argmax(k3)*deg)
-  print "Y-Translate: " + str((np.argmax(k2)-5)*8)
-  print "X-Translate: " + str((np.argmax(k1)-5)*8)
+  return (np.argmax(k3)*deg, (np.argmax(k2)-gran//2)*dist, (np.argmax(k1)-gran//2)*dist)
 
+# Number of outcomes
 
-rot = -240
-ytranslate = 8
-xtranslate = 16
+# test parameters:
+num_angles = 10 # How many angles to test
+gran = 5 # How many translations to test
+distance = 2 # Distance between translations
+trial_times = 1 # Repeat
+d_size = 4000
 
-im = fonts.RotateImage(roman_alphabet[0], rot)
-im = np.roll(im, xtranslate, axis=1)
-im = np.roll(im, ytranslate, axis=0)
+r_total = 0
+rot_correct = 0
+x_correct = 0
+y_correct = 0
 
-# rot = MSC(seventyfive, roman_alphabet[0])
-sl = X_MSC_MSC(im, roman_alphabet[0], 10000, 24)
+print ("Total potential outcomes: %d", num_angles*gran**2)
+
+for k in range(num_angles):
+  rot = -360./num_angles * k
+  rot_im = fonts.RotateImage(roman_alphabet[16], rot)
+  for _j in range(gran):
+    for _i in range(gran):
+      for _k in range(trial_times):
+        ytranslate = ((_j-gran//2)*distance)
+        xtranslate = ((_i-gran//2)*distance)
+        im = np.roll(rot_im, xtranslate, axis=1)
+        im = np.roll(im, ytranslate, axis=0)
+        # rot = MSC(seventyfive, roman_alphabet[0])
+        out = X_MSC_MSC(im, roman_alphabet[16], d_size, num_angles, gran, distance)
+        pred_rot = out[0]
+        pred_y = out[1]
+        pred_x = out[2]
+        r_total += 1
+
+        if -pred_rot == rot:
+          rot_correct += 1
+        if pred_y == -ytranslate:
+          y_correct += 1
+        if pred_x == -xtranslate:
+          x_correct += 1
+
+print "Results:"
+
+print "Rotation_correct: "+str(float(rot_correct)/r_total)
+print "Y_correct: "+str(float(y_correct)/r_total)
+print "X_correct: "+str(float(x_correct)/r_total)
 
     
--- utils/fonts.py
+++ utils/fonts.py
@@ -5,41 +5,37 @@ import skimage as sk
 import numpy as np
 import scipy.ndimage
 from skimage import feature
+from scipy import misc
 
 alpha = string.ascii_lowercase
 
-def EditFont(dir="data/font/roman2/"):
+def EditFont(dir="data/font/64/"):
   for char in alpha:
     im = skio.imread(dir+"alphabet-letter-"+char+".jpg")
     im[.8*im.shape[0]:,:] = np.max(im)
     skio.imsave("data/font/roman2edit/"+char+".jpg", im)
 
 
-def LoadFont(dir="data/font/roman2/"):
+def LoadFont(dir="data/font/64/"):
   l = []
 
   for char in alpha:
     im = skio.imread(dir+char+".jpg")
+    im = 255-im
+    im = feature.canny(im, sigma=1)
+    z = misc.imresize(im, .5)
+    im = np.zeros(im.shape)
+    length = z.shape[0]
+    x = im.shape[0]//2-length//2
+    im[x:x + length, x:x + length] = z
 
-    im[im < np.mean(im)] = np.min(im)
-    im[im > np.mean(im)] = np.max(im)
 
-    l.append(feature.canny(im))
-  s = np.array(l)
-  return s
+    l.append(im)
+  # s = np.array(l)
+  return l
 
 def RotateImage(image, angle):
-  if angle < 0:
-    angle = 360+angle
-  if angle >= 90:
-    image = np.rot90(image, angle//90)
-    angle = angle-(angle//90)*90
-  if angle == 0:
-    return image
-  x = scipy.ndimage.interpolation.rotate(image, angle, cval=np.min(image))
-  l = x.shape[0]-1
-  original = image.shape[0]
-  middle = l-image.shape[0]
-  im = x[middle/2:middle/2+original, middle/2:middle/2+original]
+  cval = image[0][0]
+  im = misc.imrotate(image, angle)
   return im
   
