c78887d7406e07a13dd023501bd26cbaa3a84db5
Quinn Tran <ssquinntran@berkeley.edu>
2017-03-06 16:36:25 -0800

    git update

--- hrr_utils.py
+++ hrr_utils.py
@@ -8,8 +8,8 @@ def GenerateDictionary(alpha, n=10000):
   return l
 
 def GenerateDefaultDictionary(n=10000):
-	alphabet = string.ascii_lowercase+"."+"#"
-	return GenerateDictionary(alphabet, n)
+  alphabet = string.ascii_lowercase+"."+"#"
+  return GenerateDictionary(alphabet, n)
 
 def GetRV(string, dic, ngram=3):
   string = "#"+string+"."
@@ -27,6 +27,32 @@ def GetRV(string, dic, ngram=3):
     tv += l
   return tv
 
-def GetRVClip(string, dic, ngram=3):
-	tv = GetRV(string,dic,ngram)
-	return np.where(tv>0, 1, -1)
+def GetRVClip(string, dic, ngram=3, fn=GetRV):
+  tv = fn(string,dic,ngram)
+  return np.where(tv>0, 1, -1)
+
+def GetRVClipHash(string, dic, ngram=3, fn=GetRV):
+  tv = fn(string,dic,ngram)
+  sh = bin(hash(string))[2:]
+  for i in range(tv.shape[0]):
+    if tv[i] == 0:
+      TF = sh[i%len(sh)] == '1'
+      tv[i] = TF * 1 + -1 * (1- TF)
+  return np.where(tv>0, 1, -1) 
+
+def GetAllRVGrams(string, dic, uptograms=3):
+  string = "#"+string+"."
+  assert len(string) >= uptograms
+
+  n = dic.values()[0].shape[0]
+  tv = np.zeros(n)
+  for ngram in range(1, uptograms+1):
+    for i in range(len(string)-ngram+1):
+      s = string[i:i+ngram]
+      l = np.ones(n)
+      for j in s:
+        l = np.roll(l, 1)
+        l = np.multiply(l, dic[j])
+          
+      tv += l
+  return tv
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def GetRegularVerbs():
+def GetRegularVerbs(frequency=False):
   """
   Returns regular verbs from cleaned/regular_verbs_clean.csv
 
@@ -8,34 +8,75 @@ def GetRegularVerbs():
   """
   present = []
   past = []
+  freq = []
 
-  f = open('cleaned/regular_verbs_clean.csv', 'r')
+
+  f = open('data/cleaned/regular_verbs_clean.csv', 'r')
   w = f.read().strip().split("\n")
   for i in w:
     k = i.split(",")
     present.append(k[0])
     past.append(k[1])
+    freq.append(int(k[2]))
+
+  if frequency:
+    return present, past, np.array(freq)
 
   return present, past
 
-def GetIrregularVerbs():
+def GetIrregularVerbs(frequency=False):
   present = []
   past = []
-  f = open('cleaned/irregular_verbs_clean.csv', 'r')
+  freq = []
+  f = open('data/cleaned/irregular_verbs_clean.csv', 'r')
   w = f.read().strip().split("\n")
   for i in w:
     k = i.split(",")
     present.append(k[0])
     past.append(k[1])
+    freq.append(int(k[2]))
+
+  if frequency:
+    return present, past, np.array(freq)
 
   return present, past
 
-def Shuffle(present, past):
+def Shuffle(present, past, freq=None):
+  if type(freq) != type(None):
+    l = zip(present, past, freq)
+    np.random.shuffle(l)
+    present, past, freq = zip(*l)
+    return present, past, freq
   l = zip(present, past)
   np.random.shuffle(l)
   present, past = zip(*l)
-  return list(present), list(past)
+  return present, past
+
 
 def LoadLatentSpace():
   f = open('cleaned/latent_space.txt')
   return f.read().strip().split("\n")
+
+def LoadLatentSpaceGroups():
+  f = open('cleaned/latent_grouped.txt')
+  k = []
+  grouping = []
+  grouped = f.read().strip().split("\n")
+  for line in grouped:
+    if len(line) == 0 or line[0] == "#":
+      continue
+    if line[0] == "!":
+      if len(grouping) > 0:
+        k.append(grouping)
+      grouping = []
+      continue
+    tokens = line.split(",")
+    grouping.append((tokens[0], tokens[1]))
+
+  if len(grouping) > 0:
+    k.append(grouping)
+
+  return k
+
+
+
