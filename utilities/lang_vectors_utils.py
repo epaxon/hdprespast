b8956f409a1207d2869665727b36ab48ed50032c
Paxon Frady <paxon.frady@gmail.com>
2017-03-08 14:15:15 -0800

    Added text to trigram dict algorithm. Moved utils folder to utilities due to import issues.

---
+++ utilities/lang_vectors_utils.py
@@ -0,0 +1,121 @@
+import random_idx
+import utils
+import numpy as np
+import string
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import sys
+
+k = 5000
+N = 10000
+# cluster_sizes is mapping to n-gram size
+# cluster_sz in random_idx referring to specific element (int) in cluster_sizes, array
+cluster_sizes = [1, 2, 3, 4, 5, 6, 7, 8]
+ordered = 1
+#assuming this is the alphabet bc of precedent in generate_text.py
+#alph = 'abc' 
+alphabet = string.lowercase + ' '
+
+# create language vector for Alice in Wonderland made of summed n-gram vectors for each
+# n in cluster_sizes
+def create_lang_vec(filename, lv, cluster_sizes, N=N, k=k):
+    
+    total_lang = np.zeros((1,N))
+    # generate english vector
+    for cz in cluster_sizes:
+        print "generating language vector of cluster size", cz
+        # which alphabet to use
+        lang_vector = random_idx.generate_RI_text_fast(N, lv, cz, ordered, filename, alphabet)#"preprocessed_texts/AliceInWonderland.txt", alph)
+        total_lang += lang_vector
+    return total_lang
+
+def initialize(filepath="preprocessed_texts/alice-only-spaced.txt"):#AliceInWonderland.txt"):
+    n_grams = []
+    # RI_letters = random_idx.generate_letter_id_vectors(N, k, alph)
+    lv = random_idx.generate_letter_id_vectors(N,15000);
+    f = open(filepath, "r");
+    text = f.read();
+    text = ''.join([x for x in text if x in alphabet])[0:10000];
+    #for each n, dictionary of key: n gram and value: frequency
+    n_gram_frequencies = [{} for _ in range(len(cluster_sizes) + 1)]
+
+    # lang_vectors in sizes 1-8
+    lang_vectors = []
+    for size in cluster_sizes:
+        lang_vectors.append(create_lang_vec(filepath, lv, [size]))
+    lang_vectors.insert(0, np.zeros((1,N)))
+
+    # save vectors to file
+    fwrite = open("intermediate/lv", "w")
+    fwrite1 = open("intermediate/lang_vectors", "w")
+    fwrite2 = open("intermediate/n_gram_frequencies", "w")
+    pickle.dump(lv, fwrite)
+    pickle.dump(lang_vectors, fwrite1)
+    pickle.dump(n_gram_frequencies, fwrite2)
+    fwrite.close()
+    fwrite1.close()
+    fwrite2.close()
+
+def initialize_from_file():
+    # save vectors to file
+    fread = open("intermediate/lv", "r")
+    fread1 = open("intermediate/lang_vectors", "r")
+    fread2 = open("intermediate/n_gram_frequencies", "r")
+    lv = pickle.load(fread)
+    lang_vectors = pickle.load(fread1)
+    n_gram_frequencies = pickle.load(fread2)
+    fread.close()
+    fread1.close()
+    fread2.close()
+    return lv, lang_vectors, n_gram_frequencies
+
+def write_data_structures(data_structures=[], file_paths=[]):
+    for i in range(0,len(file_paths)):
+        fwrite = open(file_paths[i], "w")
+        pickle.dump(data_structures[i], fwrite)
+        fwrite.close()
+
+def get_letter_vec(s, letter_vec):
+    if(len(s) == 1):
+        return letter_vec[alphabet.index(s)];
+    vec = letter_vec[alphabet.index(s[0])];
+    for i in s[1:]:
+        vec = np.multiply(np.roll(vec, 1), letter_vec[alphabet.index(i)]);
+    return vec;
+
+def recover_frequency(letter_vec, s, array):
+    vec = get_letter_vec(s, letter_vec);
+    return np.dot(vec, array[len(s)-1]);
+#should we separate by cluster sizes? following precedent, guess not
+def vocab_vector(lv, lang_vectors, filepath="preprocessed_texts/alice-only-spaced.txt"):
+    f = open(filepath, "r");
+    text = f.read()
+    text = text.split(" ")
+    #text = ''.join([x for x in text if x in alphabet])[0:10000];
+    vocab_vec = np.zeros((1,N))
+    max_length = 0
+    for word in text:
+        #print "generating vocab vector of cluster size", len(word)
+        word_vec = random_idx.id_vector(N, word, alphabet, lv, ordered)
+        vocab_vec += word_vec
+        if len(word) > max_length:
+            max_length = len(word)
+    f.close()
+    return vocab_vec, max_length
+#array with cluster size as index, the dictionary of words in that index
+def vocab(max_word_length, filepath="preprocessed_texts/alice-only-spaced.txt"):
+    f = open(filepath, "r");
+    text = f.read();
+    text = text.split(" ")
+    #text = ''.join([x for x in text if x in alphabet])[0:10000];
+    #max word length is 20 letters lol
+    array = [{} for i in range(0,max_word_length)]
+
+    for word in text:
+        if word not in array[len(word)-1].keys():
+            array[len(word)-1][word] = 1
+        else:
+            array[len(word)-1][word] += 1
+    f.close()
+    return array
---
+++ utilities/random_idx.py
@@ -0,0 +1,412 @@
+# random_idx.py
+# creates random index vectors for a number of languages
+
+# libraries
+import sys
+import numpy as np
+import string
+import utils
+import pandas as pd
+import os
+import pickle
+
+alphabet = string.lowercase + " "
+phonemes = ["AA", "AE", "AH", "AO", "AW", "AY", "B", "CH", "D", "DH", "EH", "ER", "EY", "F", "G", "HH", "IH", \
+            "IY", "JH", "K", "L", "M", "N", "NG", "OW", "OY", "P", "R", "S", "SH", "T", "TH", "UH", "UW", "V", "W", \
+            "Y", "Z", "ZH"
+           ]
+lang_dir = 'preprocessed_texts/'
+
+cluster_cache = {}
+
+
+
+k = 500
+N = 1000
+alphabet = string.lowercase + ' '
+
+
+def check_serialization(which_file):
+    if which_file == 1:
+        return os.path.isfile('letter_encoding.p')
+    else:
+        return os.path.isfile('word_dictionary.p')
+
+
+def make_serialization(make_encoding=False, make_word_dict=False):
+    if make_encoding:
+        one_hot_encoding = generate_letter_id_vectors(N, k)
+        pickle.dump(one_hot_encoding, open("letter_encoding.p", "wb"))
+    if make_word_dict:
+        word_dict = {}
+        pickle.dump(word_dict, open("word_dictionary.p", "wb"))
+
+
+def save_components(save_encoding=False, save_word_dict=False, encoding=None, dictionary=None):
+    if save_encoding:
+        pickle.dump(encoding, open("letter_encoding.p", "wb"))
+    if save_word_dict:
+        pickle.dump(dictionary, open("word_dictionary.p", "wb"))
+
+def read_components(read_encoding):
+    if read_encoding:
+        if not check_serialization(1):
+            return None
+        return pickle.load(open("letter_encoding.p", "rb"))
+    else:
+        if not check_serialization(0):
+            return None
+        return pickle.load(open("word_dictionary.p", "rb"))
+
+def fetch_serialized_encoding():
+    if not check_serialization(1):
+        print "stepping into this case"
+        make_serialization(True, False)
+    encoding = read_components(True)
+    if not check_serialization(0):
+        make_serialization(False, True)
+    word_dict = read_components(False)
+    return encoding, word_dict
+
+
+def word_hypervec(word, alph=alphabet, N=N):
+    one_hot_encoding, word_dict = fetch_serialized_encoding()
+    if word in word_dict:
+        return word_dict[word]
+    hypervector = id_vector(N, word,alph, one_hot_encoding)
+    word_dict[word] = hypervector
+    save_components(save_word_dict=True, dictionary=word_dict)
+    return hypervector
+
+def stack_list(lst):
+    all_words = []
+    for word in lst:
+        all_words.append(word_hypervec(word))
+    return np.vstack(all_words)
+
+def generate_phoneme_id_vectors(N, k, ph=phonemes):
+    # build row-wise k-sparse random index matrix
+    # each row is random index vector for phoneme
+    num_phonemes = len(ph)
+    RI_phonemes = np.zeros((num_phonemes,N))
+    for i in xrange(num_phonemes):
+            rand_idx = np.random.permutation(N)
+            RI_phonemes[i,rand_idx[0:k]] = 1
+            RI_phonemes[i,rand_idx[k:2*k]] = -1
+    return RI_phonemes
+
+def generate_letter_id_vectors(N, k, alph=alphabet):
+    # build row-wise k-sparse random index matrix
+    # each row is random index vector for letter
+    num_letters = len(alphabet)
+    RI_letters = np.zeros((num_letters,N))
+    for i in xrange(num_letters):
+            rand_idx = np.random.permutation(N)
+            RI_letters[i,rand_idx[0:k]] = 1
+            RI_letters[i,rand_idx[k:2*k]] = -1
+    return RI_letters
+
+# def generate_id(RI_letters,alph=alphabet,cluster_sz=1, ordered=0):
+#       # generate id vectors of clusters from "alphabet" with size "cluster_sz"
+#
+#       # generate clusters
+#       if ordered == 0:
+#           clusters = utils.generate_unordered_clusters(alph,cluster_sz=cluster_sz)
+#       else:
+#           clusters = utils.generate_ordered_clusters(alph,cluster_sz=cluster_sz)
+#
+#       M = len(clusters) # number of letter clusters
+#       num_letters,N = RI_letters.shape
+#       #RI_letters = generate_letter_id_vectors(N, k, alphabet)
+#
+#       RI = np.zeros((M,N))
+#       for i in xrange(M):
+#               # calculate repeats
+#               cluster = clusters[i]
+#               RI[i,:] = id_vector(N, cluster, alphabet, RI_letters, ordered=ordered)
+#       dictionary = {}
+#       for i in range(len(clusters)):
+#           dictionary[clusters[i]] = RI[i]
+#       return dictionary
+
+def generate_text_vector(N, RI_letters, cluster_sz, text_name):
+    text_vector = np.zeros((1,N))
+
+    text = utils.load_text(text_name)
+
+    for char_idx in xrange(len(text)-cluster_sz+1):
+        sidx = char_idx
+        eidx = char_idx+cluster_sz
+        
+        cluster = text[sidx:eidx]
+        
+        vector = np.ones((1,N))
+        for letter in cluster:
+            letter_idx = alphabet.find(letter)
+            vector = np.roll(vector, 1)
+            vector = np.multiply(vector, RI_letters[letter_idx, :])
+            
+        text_vector += vector
+    return text_vector / (len(text)-cluster_sz+1)
+    
+ 
+"""
+Returns lang_vec and then vocab_vec
+
+min-max will be a dot product value, so it
+should be a multiple of 10k.
+"""
+def generate_vocab_lang_vectors(N, RI_letters, cluster_sz, ordered, text_name, min_, max_,alph=alphabet):
+    text_vector = np.zeros((1, N))
+    vocab_vec = np.zeros((1,N))
+    text = utils.load_text(text_name)
+    cluster = ''
+    vector = np.ones((1,N))
+    for char_num in xrange(len(text)):      
+        cluster = cluster + text[char_num]
+        if len(cluster) < cluster_sz:
+            continue
+        elif len(cluster) > cluster_sz:
+            prev_letter = cluster[0]
+            prev_letter_idx = alphabet.find(prev_letter)
+            inverse = np.roll(RI_letters[prev_letter_idx,:], cluster_sz-1)
+            vector = np.multiply(vector, inverse)
+            vector = np.roll(vector, 1)
+            letter = text[char_num]
+            letter_idx = alphabet.find(letter)
+            vector = np.multiply(vector, RI_letters[letter_idx,:])
+            cluster = cluster[1:]
+        else: # (len(cluster) == cluster_size), happens once
+            letters = list(cluster)
+            for letter in letters:
+                vector = np.roll(vector,1)
+                letter_idx = alphabet.find(letter)
+                vector = np.multiply(vector, RI_letters[letter_idx,:])
+
+        if(np.dot(vocab_vec, vector.T)<-10000):
+            text_vector += vector;
+
+        else:
+            vocab_vec+= vector;
+            if(np.dot(vocab_vec, vector.T) > min_):
+                while(True):
+                    if(np.dot(vocab_vec, vector.T)<-10000):
+                        break;
+                    else:
+                        vocab_vec -= vector;
+
+
+    return text_vector, vocab_vec
+
+def id_vector(N, cluster, alphabet, RI_letters,ordered=0):
+        if cluster == '':
+                        return 0
+        if ordered == 0:
+                # unordered clusters
+                cluster = ''.join(sorted(cluster))
+
+#if it's already calculated, just return it
+        if cluster in cluster_cache:
+                return cluster_cache[cluster]
+                
+        vector = np.zeros(N)
+        
+                        
+        # ordered clusters
+        letters = list(cluster)
+        prod = np.ones((1,N))
+        roller = len(letters)-1
+        for letter in letters:
+                        # working code
+                        #letter_idx = alphabet.find(letter)
+                        #prod = np.multiply(prod, RI_letters[letter_idx,:])
+                        #prod = np.roll(prod,1)
+                        
+                        # testing permutations
+                        letter_idx = alphabet.find(letter)
+                        prod = np.multiply(prod, np.roll(RI_letters[letter_idx, :],roller))
+                        roller -= 1
+        vector = prod
+
+        if len(cluster_cache) > 100000:
+                cluster_cache.clear()
+                print "clearing cache"
+        cluster_cache[cluster] = vector
+        return vector
+
+def generate_RI_str(N, RI_letters, cluster_sz, ordered, string, alph=alphabet):
+        # generate RI vector for string
+
+        text_vector = np.zeros((1,N))
+        for char_num in xrange(len(string)):
+
+                if char_num < cluster_sz:
+                        continue
+                else:
+                        # build cluster
+                        cluster = ''
+                        for j in xrange(cluster_sz):
+                                cluster = string[char_num - j] + cluster
+                        text_vector += id_vector(N, cluster, alph,RI_letters, ordered)
+        return text_vector
+
+def generate_RI_text(N, RI_letters, cluster_sz, ordered, text_name, alph=alphabet):
+        # generate RI vector for "text_name"
+        # assumes text_name has .txt
+
+        text_vector = np.zeros((1, N))
+        text = utils.load_text_spaces(text_name)
+        for char_num in xrange(len(text)):
+
+                if char_num < cluster_sz:
+                        continue
+                else:
+                        # build cluster
+                        cluster = ''
+                        for j in xrange(cluster_sz):
+                                cluster = text[char_num - j] + cluster
+                        print cluster
+                        text_vector += id_vector(N, cluster, alph,RI_letters, ordered)
+        return text_vector
+
+def generate_RI_sentence(N, RI_letters, cluster_sz, ordered, text, alph=alphabet):
+        # generate RI vector for "text_name"
+        # assumes text_name has .txt
+
+        text_vector = np.zeros((1, N))
+        ##text = utils.load_text_spaces(text_name)
+        for char_num in xrange(len(text)):
+
+                if char_num < cluster_sz:
+                        continue
+                else:
+                        # build cluster
+                        cluster = ''
+                        for j in xrange(cluster_sz):
+                                cluster = text[char_num - j] + cluster
+                        text_vector += id_vector(N, cluster, alph,RI_letters, ordered)
+        return text_vector
+
+    #Not really fast. Theoretically faster, but not for real (not using cache)  
+def generate_RI_text_fast(N, RI_letters, cluster_sz, ordered, text_name, alph=alphabet):
+    text_vector = np.zeros((1, N))
+    text = text_name # utils.load_text(text_name)
+    cluster = ''
+    vector = np.ones((1,N))
+    for char_num in xrange(len(text)):    
+        if (char_num % 1000) == 0:
+            print char_num,
+            
+        cluster = cluster + text[char_num]
+        print cluster
+        if len(cluster) < cluster_sz:
+            continue
+        elif len(cluster) > cluster_sz:
+            prev_letter = cluster[0]
+            prev_letter_idx = alphabet.find(prev_letter)
+            inverse = np.roll(RI_letters[prev_letter_idx,:], cluster_sz-1)
+            vector = np.multiply(vector, inverse)
+            vector = np.roll(vector, 1)
+            letter = text[char_num]
+            letter_idx = alphabet.find(letter)
+            vector = np.multiply(vector, RI_letters[letter_idx,:])
+            cluster = cluster[1:]
+        else: # (len(cluster) == cluster_size), happens once
+            print cluster
+            letters = list(cluster)
+            for letter in letters:
+                vector = np.roll(vector,1)
+                letter_idx = alphabet.find(letter)
+                vector = np.multiply(vector, RI_letters[letter_idx,:])
+        text_vector += vector
+    return text_vector
+
+def generate_RI_text_words(N, RI_letters, text_name, alph=alphabet):
+        # generate RI vector for "text_name"
+        # assumes text_name has .txt
+
+        text_vector = np.zeros((1, N))
+        text = utils.load_text_spaces(text_name)
+        cluster = ''
+        for char_num in xrange(len(text)):
+                char = text[char_num]
+                if char == ' ':
+                        text_vector += id_vector(N, cluster, alph, RI_letters)
+                        # reset cluster
+                        cluster = ''
+                else:
+                        cluster += text[char_num]
+        return text_vector
+
+def generate_RI_text_history(N, RI_letters, text_name, alph=alphabet):
+        # generate RI vector for "text_name"
+        # assumes text_name has .txt
+
+        text_vector = np.zeros((1, N))
+        history_vector = np.zeros((1,N))
+        text = utils.load_text(text_name)
+        for char_num in xrange(len(text)):
+            char = text[char_num]
+            letter_idx = alphabet.find(char)
+            history_vector = 0.75*history_vector + RI_letters[letter_idx,:]
+            text_vector += history_vector   
+                
+        return text_vector
+
+def generate_RI_lang(N,RI_letters, cluster_sz, ordered, languages=None):
+
+        cluster_cache.clear()
+
+        if languages == None:
+                languages = ['english','german','norwegian','finnish']
+
+
+        num_lang = len(languages)
+
+        lang_vectors = np.zeros((num_lang,N))
+
+        for i in xrange(num_lang):
+                # load text one at a time (to save mem), English, German, Norwegian
+                print 'loading ' + languages[i]
+                lang_vectors[i,:] = generate_RI_text(N, RI_letters, cluster_sz, ordered, lang_dir + languages[i] + '.txt')
+
+        return lang_vectors
+
+
+def generate_RI_lang_history(N,RI_letters, languages=None):
+
+        cluster_cache.clear()
+
+        if languages == None:
+                languages = ['english','german','norwegian','finnish']
+
+
+        num_lang = len(languages)
+
+        lang_vectors = np.zeros((num_lang,N))
+
+        for i in xrange(num_lang):
+                print 'loading ' + languages[i]
+                # load text one at a time (to save mem), English, German, Norwegian
+                lang_vectors[i,:] = generate_RI_text_history(N, RI_letters, lang_dir + languages[i] + '.txt')
+
+        return lang_vectors
+
+def generate_RI_lang_words(N, RI_letters, languages=None):
+
+        cluster_cache.clear()
+
+        if languages == None:
+                languages = ['english','german','norwegian','finnish']
+
+
+        num_lang = len(languages)
+
+        lang_vectors = np.zeros((num_lang,N))
+
+        for i in xrange(num_lang):
+                print 'loading ' + languages[i]
+                # load text one at a time (to save mem), English, German, Norwegian
+                lang_vectors[i,:] = generate_RI_text_words(N, RI_letters, lang_dir + languages[i] + '.txt')
+
+        return lang_vectors
