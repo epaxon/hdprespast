d6ea58a58c8c9ce51e175480e68f5f97daa9ee4e
Quinn Tran <ssquinntran@berkeley.edu>
2017-12-01 13:51:34 -0800

    um

--- RM_utils.py
+++ RM_utils.py
@@ -45,7 +45,7 @@ def ngram_encode(ngram_str, letter_vecs, alph):
 
 def ngram_encode_cl(ngram_str, letter_vecs, alph):
     vec = ngram_encode(ngram_str, letter_vecs, alph)
-    
+
     return 2* (vec + 0.1*(np.random.rand(letter_vecs.shape[1])-0.5) > 0) - 1
 
 def genX(verbs, N, dic1, dic2, alph):
@@ -57,6 +57,7 @@ def genX(verbs, N, dic1, dic2, alph):
     
     for m in range(len(verbs)):
         pair = verbs[m]
+
         PRES1[m] = ngram_encode_cl(pair[1], dic1, alph)
         PRES2[m] = ngram_encode_cl(pair[1], dic2, alph)
         PAST1[m] = ngram_encode_cl(pair[0], dic1, alph)
@@ -124,12 +125,6 @@ def outer_reg_train_diff(W, past2, present1, present2, sim, N):
     pred = np.dot(W, present1) + present2*N
     W += ((N*N-sim(pred, past2))/float(N*N)) * np.outer(present1, past2-present2)
     return W
-
-def closed_train(Past, Present):
-    return np.dot(np.linalg.pinv(Present), Past)
-
-def cap_closed_train(Past, Present):
-    return np.dot(np.linalg.pinv(Present,rcond=5e-2), Past)
 
 def sim(x, y):
     if len(x.shape) == 1 or len(y.shape)==1:
@@ -206,6 +201,49 @@ def graph(x, y1, y2, y1std, y2std, accuracy, irreg_accuracy, title=None, xlabel=
 
     plt.tight_layout()
 
+    plt.subplot(212)
+    plt.plot(x, accuracy, c='b', label='Regular')
+    plt.plot(x, irreg_accuracy, c='g', label='Irregular')
+    plt.xlabel('Number Training Examples',fontsize=16)
+    plt.ylabel('Accuracy',fontsize=16)
+    plt.legend(loc=legend,fontsize=12)
+    
+# https://matplotlib.org/api/lines_api.html
+def graph_separate(x, y1, y2, y1std, y2std, \
+                   y1irregular, y2irregular, \
+                   accuracy, irreg_accuracy, \
+                   title=None, xlabel='number of words', ylabel='average dot product', legend='upper left'):   
+    plt.figure(1)
+    plt.subplot(211)
+    fig = plt.figure(figsize=(4,3))
+    plt.tick_params(axis='both', which='major', labelsize=12)
+    plt.tick_params(axis='both', which='minor', labelsize=12)
+    
+    start = round_to_tick(min(min(y1), min(y2)) - max(max(y1std),max(y2std)))
+    end = round_to_tick(max(max(y1), max(y2)) + max(max(y1std),max(y2std)))
+    
+    y1std = np.clip(y1std, max(-8, start-.5), min(8, end+.5))
+    y2std = np.clip(y2std, max(-8, start-.5), min(8, end+.5))
+    
+
+    plt.plot(x, y1, c='b', lw=2, label='Train')
+    plt.plot(x, y2, c='g', lw=2, label='Test')
+    plt.plot(x, y1irregular, ':', c='b', lw=2, label='Train irregular')
+    plt.plot(x, y2irregular, ':', c='g', lw=2, label='Test irregular')
+
+    plt.fill_between(x, y1-y1std, y1+y1std, facecolor='b', alpha=0.1)
+    plt.fill_between(x, y2-y2std, y2+y2std, facecolor='g', alpha=0.1)
+
+    plt.legend(loc=legend,fontsize=12)
+
+    plt.xlabel('Number Training Examples',fontsize=16)
+    plt.ylabel('Feature Similarity',fontsize=16)
+    plt.title(title,fontsize=16)
+
+    plt.xlim([0, 1700])
+
+    plt.tight_layout()
+    
     plt.subplot(212)
     plt.plot(x, accuracy, c='b', label='Regular')
     plt.plot(x, irreg_accuracy, c='g', label='Irregular')
